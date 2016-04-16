9e2ad719199c3aa4e77bd7d82178ac81eb879d76
CharlieBr <kasia_glab@wp.pl>
2016-04-16 18:39:00 +0200

    add Dice

---
+++ pl.edu.agh.io.chinczyk/src/main/java/pl/edu/agh/io/chinczyk/logic/Dice.java
@@ -0,0 +1,11 @@
+package pl.edu.agh.io.chinczyk.logic;
+
+import java.util.Random;
+
+public class Dice {
+    private static final Random random = new Random();
+
+    public int throwDice(){
+        return random.nextInt(6) + 1;
+    }
+}
