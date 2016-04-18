a617bbea88be410e0fdb4a15c1d65010b49b2938
Nina <nina.pikulska@gmail.com>
2016-04-18 09:16:05 +0200

    Players should have own maps

--- pl.edu.agh.io.chinczyk/src/main/java/logic/Player.java
+++ pl.edu.agh.io.chinczyk/src/main/java/logic/Player.java
@@ -1,6 +1,19 @@
 package logic;
 
+import java.awt.*;
+
 public class Player {
+	private Map map;
+	private Color color;
+
 	public Player(int i){
+	}
+
+	public Map getMap() {
+		return map;
+	}
+
+	public void setMap(Map map) {
+		this.map = map;
 	}
 }
